0e6c95f8ba6c7eccfd0cf18f55a821fe5ee7e302
vinvi <vinvi@VIN-PC>
2020-04-15 23:25:55 +0530

    Test.

---
+++ SeleniumWorkout/src/main/java/plainseleniumscripts/NykaaClass.java
@@ -0,0 +1,29 @@
+package plainseleniumscripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+ 
+public class NykaaClass { 
+	
+	public static RemoteWebDriver driver; 
+
+	public static void main(String[] args) { 
+		
+		// Launching the application --
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe"); 
+		ChromeOptions options = new ChromeOptions(); 
+		options.addArguments("--disable-notifications"); 
+		driver = new ChromeDriver(options); 
+		driver.get("https://www.nykaa.com/"); 
+		driver.manage().window().maximize(); 
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); 
+		
+		// Selection the L'Oreal Paris brand 
+		
+
+	}
+
+}
